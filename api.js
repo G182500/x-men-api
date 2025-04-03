cb72818177556c718668b9fe04d6caef51315a2a
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2025-04-03 16:53:24 -0300

    refactor: remove comments

--- api.js
+++ api.js
@@ -26,30 +26,10 @@ app.post('/user', createUser);
 
 app.get('/mutant', getMutant);
 
-/*
-app.post('/user', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    
-    if (!email || !password || !username) return res.status(400).json({ message: 'username, email and password required' });
+// const hashedPassword = await bcrypt.hash(password, 10); 
+/* Salt rounds é uma camada de proteção, onde cada round deixa o hash mais forte. Quanto mais rounds, mais seguro e mais demorado.
+Menos rounds, mais rápido e mais arriscado. 10 é o padrão.
 
-    const user = await checkEmail(email, db);
-    if (user) return res.status(422).json({ message: 'email already registered' });
-  
-    //const query = await db.promise().query('INSERT INTO users (username, email, password_hash) values (?, ?, ?)', [username, email, password]);
-    //console.log(query);
-
-  } catch(err) {
-    console.error(err);
-    res.status(500).json({ message: 'server error' });
-  }
-})*/
-
-  // const hashedPassword = await bcrypt.hash(password, 10); 
-  /* Salt rounds é uma camada de proteção, onde cada round deixa o hash mais forte. Quanto mais rounds, mais seguro e mais demorado.
-  Menos rounds, mais rápido e mais arriscado. 10 é o padrão. */
-
-/*
 app.get('/', async (req, res) => {
     res.send('<h1 style="color: green">🔥 Servidor dos X-MEN está rodando!</h1>');
 });*/
